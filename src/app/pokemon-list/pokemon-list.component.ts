0232f63477089c4e6991e732ea6fb450286d8b5f
Camilo Henrique <camilo.henri.ch@gmail.com>
2023-10-25 13:34:22 -0300

    feat: pokemon-card

--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Pokemon } from '../../_model/Pokemon';
+import { Type } from '../../_model/Type';
 
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
-  styleUrls: ['./pokemon-list.component.sass']
+  styleUrls: ['./pokemon-list.component.sass'],
 })
 export class PokemonListComponent {
-
+  public pokemons: Pokemon[] = [
+    {
+      image:
+        'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png',
+      name: 'Bulbassalto',
+      number: 1,
+      types: [Type.Grass, Type.Poison],
+    },
+    {
+      image:
+        'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png',
+      name: 'Bulbassalto',
+      number: 1,
+      types: [Type.Grass, Type.Poison],
+    },
+  ];
 }
+
